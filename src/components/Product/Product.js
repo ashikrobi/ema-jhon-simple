adcc849ee19d4aeb9140d5f990b12397934b005e
Ashiqur Rahman <ashikrobi@gmail.com>
2021-02-27 12:10:58 +0600

    Added button and cart icon with font awesome

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
 const Product = (props) => {
-    // console.log(props.product);
+    console.log(props.product);
+    const {img, name, seller, price, stock} = props.product;
     return (
-        <div>
-            <h4>{props.product.name}</h4>
+        <div className="product">
+            <div>
+                <img src={img} alt=""/>
+            </div>
+            <div>
+                <h4 className='product-name'>{name}</h4>
+                <br/>
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>only {stock} left in stock - order soon</small></p>
+                <button className='main-button'><FontAwesomeIcon icon={faShoppingCart}/> add to cart</button>
+            </div>
         </div>
     );
 };
